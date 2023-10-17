d6ef04fa48a845ec5124078865aa780e7b2bc5f7
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-10-17 16:52:42 -0300

    fix: fix usuario_role

--- src/models/Usuario_roles.js
+++ src/models/Usuario_roles.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const usuariosRoles = new mongoose.Schema({
-  id: { type: String },
   usuario_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "usuario",
